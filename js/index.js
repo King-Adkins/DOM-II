db265c3e3eb9b55ff12f1bd0c0ea01844f1ac4e2
King Adkins <lambda_school_loaner_87@Lambda-School-Loaner-87s-MacBook-Pro.local>
2019-04-09 12:59:59 -0700

    Add some events to fun bus page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+const destinationBtn = document.querySelectorAll('.destination .btn');
+
+for(let i = 0; i < destinationBtn.length; i++) {
+destinationBtn[i].addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'red';
+});
+
+destinationBtn[i].addEventListener('mouseover', (event) => {
+    event.target.style.color = 'purple' ;  
+});
+
+destinationBtn[i].addEventListener('dblclick', (event) => {
+    event.target.style.backgroundColor = 'pink';
+});
+}
+
+window.addEventListener('resize', (event) => {
+    alert('Wait.... NO STOP resizing me!');
+});
+
