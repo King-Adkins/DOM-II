4c75a9130d79ee863f982b1db829899fc6dead8a
King Adkins <lambda_school_loaner_87@Lambda-School-Loaner-87s-MacBook-Pro.local>
2019-04-09 13:48:38 -0700

    Add all events to fun bus page

--- js/index.js
+++ js/index.js
@@ -19,3 +19,37 @@ window.addEventListener('resize', (event) => {
     alert('Wait.... NO STOP resizing me!');
 });
 
+const logo = document.querySelector('.logo-heading');
+logo.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'red';
+});
+
+const intro = document.querySelector('.intro h2');
+intro.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'blue';
+});
+
+const content = document.querySelector('.text-content h2');
+content.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'green';
+});
+
+const adv = document.querySelector('.text-content p');
+adv.addEventListener('mouseover', (event) => {
+    event.target.style.fontSize = '3rem';
+    
+});
+
+const header3 = document.querySelector('.text-content h2');
+header3.addEventListener('mouseout', (event) => {
+    event.target.style.color = 'orange';
+    event.stopPropagation();
+})
+
+const aTag = document.querySelector('a');
+aTag.addEventListener('click', function(event){
+alert("Click on something else man...");
+// This will prevent the form from submitting to action_page.php
+ event.preventDefault();
+});
+
